fb9cac69688620b6e8f1b2ff5906658ac6caf312
KAUAN RODRIGUES GALDINO <kauan.rgaldino@senacsp.edu.br>
2023-02-02 09:07:21 -0300

    FUNÇÕES DE JAVASCRIPT

---
+++ funcao.js
@@ -0,0 +1,22 @@
+/*function exemplo(){
+   console.log("Perigosa") 
+}
+
+exemplo()
+*/
+
+/*function exemplo() {
+    console.log("Perigosa")
+    console.log("Perigosa")
+    console.log("Perigosa")
+    return retorno;
+}
+
+exemplo()
+*/
+
+function multiplicar(a, b = 1) {
+    return a*b;
+  }
+  
+  multiplicar(5); // 5                                                                                                                                                                                                                                                                                                                                                                           
